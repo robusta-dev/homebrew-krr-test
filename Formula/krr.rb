1fa99435f474a074400235a09d12119dc3b8c5f6
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2023-05-24 16:36:38 +0300

    Update mac hashsum

--- Formula/krr.rb
+++ Formula/krr.rb
@@ -1,15 +1,15 @@
 class Krr < Formula
     desc "Kubernetes Resource Recommender by Robusta"
     homepage "https://github.com/robusta-dev/krr"
-  
+
     if OS.mac?
-      url "https://github.com/robusta-dev/krr/releases/download/v1.1.0/krr-macos-v1.1.0.zip"
-      sha256 "5bcda1456cdb1e4f5e047084182e051164f2da32f3a9392463a9210deaa44408"
+        url "https://github.com/robusta-dev/krr/releases/download/v1.1.0/krr-macos-v1.1.0.zip"
+        sha256 "12228ba125aeba297d1d256a663e28f089dc35cdb318c5be7eb9d90f675bf1b4"
     elsif OS.linux?
-      url "https://github.com/robusta-dev/krr/releases/download/v1.1.0/krr-linux-v1.1.0.zip"
-      sha256 "12712e6009a445da4f0564fc913eba444fd29b670fddb201bb8db13ed5861aef"
+        url "https://github.com/robusta-dev/krr/releases/download/v1.1.0/krr-linux-v1.1.0.zip"
+        sha256 "12712e6009a445da4f0564fc913eba444fd29b670fddb201bb8db13ed5861aef"
     end
-  
+
     def install
         libexec.install Dir["*"]
         bin.write_exec_script (libexec/"krr")
@@ -19,4 +19,3 @@ class Krr < Formula
         system "#{bin}/krr", "version"
     end
 end
-  
